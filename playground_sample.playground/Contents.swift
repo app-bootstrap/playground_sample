efea585fa6cf4240c81769f051a4f84cedd265a2
xudafeng <xudafeng@126.com>
2016-01-16 19:27:07 +0800

    first commit

---
+++ playground_sample.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let colors = [UIColor.redColor() , UIColor.yellowColor(), UIColor.blueColor()];
+
+//colors.count;
+
+let WIDTH = 100;
+let HEIGHT = 40;
+var backgroundLable = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: WIDTH, height: HEIGHT * colors.count)));
+backgroundLable.backgroundColor = UIColor.blackColor();
+
+var index: Int;
+index = 0;
+
+for (color) in colors {
+    let colorLable = UILabel(frame: CGRect(origin: CGPoint(x: 0, y: index * HEIGHT), size: CGSize(width: WIDTH, height: HEIGHT)));
+    colorLable.backgroundColor = color;
+    backgroundLable.addSubview(colorLable);
+    index++;
+}
+
+backgroundLable
